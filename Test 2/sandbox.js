1f733b8bf2a2bfc47da2849b3e2234176ddddcfe
aminMahjub <aminmahjoob3172@gmail.com>
2021-07-27 15:16:16 +0430

    edit tests and create README

--- Test 2/sandbox.js
+++ Test 2/sandbox.js
@@ -1,27 +1,3 @@
-const opposite = realNumber => {
-    let number = Math.trunc(+Number);
-    let resualtNumber = Math.sign(number);
-
-    switch (resualtNumber) {
-        case -1:
-            return Math.abs(number);
-            break;
-        case 1:
-            return -number;
-            break;
-        case 0:
-            return number;
-            break;
-        default:
-            console.log('Your number is not valid');
-    }
-    
-}
-
-opposite(realNumber);
-
-let oppositeTwo = realNumberTwo => {
-    return number > 0 ? -number : Math.abs(number)
-}
+let oppositeTwo = realNumberTwo => realNumberTwo > 0 ? -realNumberTwo : Math.abs(realNumberTwo)
 
 oppositeTwo(realNumberTwo);
